c8a3884f02009609788b0f067b33d34490e2ff85
nwafor6 <nwaforglory6@gmail.com>
2023-11-01 15:56:08 +0100

    corrected profileimage storrage from the login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -39,7 +39,7 @@ const Login =()=>{
                 username:data.user.username,
                 id:data.user._id
             }
-            const myPics=data.profileImage
+            const myPics=data.user.profileImage
             localStorage.setItem("myPics", JSON.stringify(myPics))
             localStorage.setItem("user", JSON.stringify(user))
             console.log(data.user)
--- src/components/apiConfig.jsx
+++ src/components/apiConfig.jsx
@@ -1,3 +1,3 @@
-const BASE_URL = 'https://mytaskz.onrender.com'; // Your API base URL
-// const BASE_URL = 'http://localhost:8000'; // Your API base URL
+// const BASE_URL = 'https://mytaskz.onrender.com'; // Your API base URL
+const BASE_URL = 'http://localhost:8000'; // Your API base URL
 export default BASE_URL;
